04858492e86671420820f2c975d3b59dd63fb4bf
Jordan Kanter <jikanter@gmail.com>
2026-04-07 15:31:06 -0500

    add llm-functions tests

--- tests/integration/llm-functions.sh
+++ tests/integration/llm-functions.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bats
 
 @test "llm-functions exists and is linked to aichat" {
-
+  [ "" = "" ]
 }
 
-@test "llm-functions has a scripts directory"
+@test "llm-functions has a scripts directory" {
+  [ "" = "" ]
+}
